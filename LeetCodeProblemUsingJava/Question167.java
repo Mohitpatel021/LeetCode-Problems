22c73e1ff2ce719139b052156021f81491edd60c
Mohitpatel021 <mohitpa021@gmail.com>
2025-06-14 20:22:02 +0530

    Added one more question using python and java

---
+++ LeetCodeProblemUsingJava/Question167.java
@@ -0,0 +1,24 @@
+package LeetCodeProblemUsingJava;
+
+
+public class Question167 {
+
+  public int[] twoSum(int[] numbers, int target) {
+    int[] arr = new int[2];
+    int start = 0;
+    int end = numbers.length - 1;
+    while (start < end) {
+      int sum = numbers[start] + numbers[end];
+      if (sum == target) {
+        arr[0] = ++start;
+        arr[1] = ++end;
+        break;
+      } else if (sum < target) {
+        start++;
+      } else {
+        end--;
+      }
+    }
+    return arr;
+  }
+}
