c3897f2a984dc69b4ddd2504d398fe6e0ccee0e1
Mohitpatel021 <mohitpa021@gmail.com>
2025-06-18 23:17:45 +0530

    Solved One more question : 392 :18/06/25

---
+++ LeetCodeProblemUsingJava/Question392.java
@@ -0,0 +1,18 @@
+package LeetCodeProblemUsingJava;
+
+//  Question 392 : Is Subsequence
+//  Link :  https://leetcode.com/problems/is-subsequence/description/
+
+public class Question344 {
+    public boolean isSubsequence(String s, String t) {
+        int i=0,j=0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+                j++;}
+            else j++;
+        }
+        return i==s.length();
+    
+}
+}
