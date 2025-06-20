08099f5d49ae5af9b0955f6e44db36bb566e202d
Mohitpatel021 <mohitpa021@gmail.com>
2025-06-20 23:04:56 +0530

    Addedd one more question :2108 :20/06/2025

--- LeetCodeProblemUsingJava/Question2108.java
+++ LeetCodeProblemUsingJava/Question2108.java
@@ -4,5 +4,21 @@ package LeetCodeProblemUsingJava;
 // Link : https://leetcode.com/problems/find-first-palindromic-string-in-the-array/description/
 
 public class Question2108 {
-    
+
+  public String firstPalindrome(String[] words) {
+    for (String s : words) {
+      int i = 0;
+      int j = s.length() - 1;
+      while (i <= j) {
+        if (s.charAt(i) == s.charAt(j)) {
+          i++;
+          j--;
+        } else break;
+      }
+      if (i > j) {
+        return s;
+      }
+    }
+    return "";
+  }
 }
