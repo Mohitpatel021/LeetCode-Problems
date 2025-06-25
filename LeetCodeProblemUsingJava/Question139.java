157da173d45349c62663c071a1c46f9fc1f459c3
Mohitpatel021 <mohitpa021@gmail.com>
2025-06-25 22:54:01 +0530

    Aded a new question 139:25/06/25

---
+++ LeetCodeProblemUsingJava/Question139.java
@@ -0,0 +1,22 @@
+package LeetCodeProblemUsingJava;
+
+// Question 139 :Word Break
+// Link : https://leetcode.com/problems/word-break/description/
+
+public class Question139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp=new boolean[s.length()+1];
+        dp[0]=true;
+        Set<String> set = new HashSet<>(wordDict); //m
+        for(int i=1;i<=s.length();i++){ //n
+            for(int j=0;j<i;j++){ //n
+                String word = s.substring(j,i); //n
+                if(set.contains(word)&&dp[j]==true){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
