c8c1764b58a01dc1b9953009dc180fe5f65730be
Mohitpatel021 <mohitpa021@gmail.com>
2025-08-03 22:56:43 +0530

    Added Linked list quest 03/08/2025

---
+++ LeetCodeProblemUsingJava/Question2095.java
@@ -0,0 +1,36 @@
+package LeetCodeProblemUsingJava;
+
+
+// Question 2095. Delete the Middle Node of a Linked List
+// Link : https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
+
+public class Question2095 {
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head==null || head.next==null){
+            return null;
+        }
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=slow.next;
+        slow.next=null;
+        return head;
+    }
+}
+}
